e9da8f6807e5752eb750580edab3097bfd9400ab
hugojing <toloric@163.com>
2016-08-01 10:52:13 +0800

    更改 README，准备开源

--- README.md
+++ README.md
@@ -1,19 +1,58 @@
-# toolbox
+# Toolbox
 
-## How to start
+一个小型的项目，帮助新手体会一下如何做出可以看、可以用的原生界面。
 
-### Install
+## 包含内容
+
+- div、input、text、image、wxc-button 等组件
+- 组件分割与嵌套
+- repeat 这样的显示逻辑控制方式
+- onclick、oninput 这样的事件
+- `{{  }}` 将数据插入模板
+- Stream 模块的 fetch API 网络请求
+- Modal 模块的 alert API 警告框
+
+## 如何使用
+
+### 安装
 
 ```bash
 npm install
 ```
 
-### Development
+### 预览
 
-* `npm run build`: build `src/main.we` into `dist/main.js`
-* `npm run dev`: watch file changes of `src/main.we` and automatically build into `dist/main.js`
-* `npm run serve`: preview in html5 renderer through `http://localhost:8080/`
+* `npm run serve`: HTML 预览 `http://localhost:8080/`
 
-*note: the entry file can be configured in `webpack.config.js`, learn more from [weex-loader](https://www.npmjs.com/package/weex-loader)*
+![1]('https://raw.githubusercontent.com/hugojing/toolbox/master/images/1.png')
 
-Finally the generated code will be found in `dist` folder.
+`http://localhost:8080/index.html?page=./dist/translate.js`
+
+![2]('https://raw.githubusercontent.com/hugojing/toolbox/master/images/2.png')
+![3]('https://raw.githubusercontent.com/hugojing/toolbox/master/images/3.png')
+
+* 在 src 目录下 `weex main.we --qr`: Weex Playground 预览 首页
+
+![4]('https://raw.githubusercontent.com/hugojing/toolbox/master/images/4.png')
+* 在 src 目录下 `weex translate.we --qr`: Weex Playground 预览 翻译页
+
+![5]('https://raw.githubusercontent.com/hugojing/toolbox/master/images/4.png')
+![6]('https://raw.githubusercontent.com/hugojing/toolbox/master/images/4.png')
+
+（Weex Playground 预览，需要安装 [weex-toolkit](http://alibaba.github.io/weex/doc/tools/cli.html) 才能使用 weex 命令；手机上需要使用 [Weex Playground app](http://alibaba.github.io/weex/download.html) 来扫描二维码）
+
+### 开发
+
+开发目录：`src`
+
+输出目录：`dist`
+
+* `npm run devserve`: 开发时热加载并 HTML 预览： `http://localhost:8080/` （改动后自动更新，但需手动刷新浏览器）
+* `npm run dev`: 监视两个 .we 文件的改动，并自动打包输出为两个 .js 文件
+* `npm run build`: 将两个 .we 文件打包为两个 .js 文件
+
+（详情请参阅 `webpack.config.js` 和 `package.json`）
+
+## 服务
+
+有啥疑问，提 Issue 吧，我尽量帮你解答 :D
---
+++ dist/main.js
@@ -0,0 +1,352 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	;__weex_define__("@weex-component/bce66e635f1cba88312887763c1589cd", [], function(__weex_require__, __weex_exports__, __weex_module__){
+
+	;
+	  __webpack_require__(1)
+	  __webpack_require__(2)
+	  __webpack_require__(3)
+	  __weex_module__.exports = {
+	    data: function () {return {}},
+	  }
+
+	;__weex_module__.exports.template = __weex_module__.exports.template || {}
+	;Object.assign(__weex_module__.exports.template, {
+	  "type": "div",
+	  "classList": [
+	    "bg"
+	  ],
+	  "children": [
+	    {
+	      "type": "logo",
+	      "classList": [
+	        "logo"
+	      ]
+	    },
+	    {
+	      "type": "searchbar",
+	      "classList": [
+	        "searchbar"
+	      ]
+	    },
+	    {
+	      "type": "common",
+	      "classList": [
+	        "common"
+	      ]
+	    }
+	  ]
+	})
+	;__weex_module__.exports.style = __weex_module__.exports.style || {}
+	;Object.assign(__weex_module__.exports.style, {
+	  "bg": {
+	    "backgroundColor": "#F2F2F2"
+	  },
+	  "common": {
+	    "position": "absolute",
+	    "bottom": 10
+	  }
+	})
+	})
+	;__weex_bootstrap__("@weex-component/bce66e635f1cba88312887763c1589cd", {
+	  "transformerVersion": "0.3.1"
+	},undefined)
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	;__weex_define__("@weex-component/searchbar", [], function(__weex_require__, __weex_exports__, __weex_module__){
+
+	;
+	  __weex_module__.exports = {
+	    data: function () {return {}},
+	  }
+
+	;__weex_module__.exports.template = __weex_module__.exports.template || {}
+	;Object.assign(__weex_module__.exports.template, {
+	  "type": "div",
+	  "children": [
+	    {
+	      "type": "input",
+	      "classList": [
+	        "searchbar"
+	      ],
+	      "attr": {
+	        "type": "text",
+	        "placeholder": "一个输入框"
+	      }
+	    }
+	  ]
+	})
+	;__weex_module__.exports.style = __weex_module__.exports.style || {}
+	;Object.assign(__weex_module__.exports.style, {
+	  "searchbar": {
+	    "height": 100,
+	    "padding": 10,
+	    "marginLeft": 20,
+	    "marginRight": 20,
+	    "fontSize": 30,
+	    "borderRadius": 5,
+	    "backgroundColor": "#ffffff"
+	  }
+	})
+	})
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	;__weex_define__("@weex-component/common", [], function(__weex_require__, __weex_exports__, __weex_module__){
+
+	;
+	  __weex_module__.exports = {
+	    data: function () {return {
+	      root: 'dist',
+	      item: { name: 'translate' },
+	      tools: ['http://hbimg.b0.upaiyun.com/72fa4f426ccae79476bbbc2ef723c388cf5f02ffeb96-zYEnwV_fw658',
+	              'http://hbimg.b0.upaiyun.com/028bb24be05e2eac593340d850457051df0ba5af9963-FALRwW_fw658',
+	              'http://hbimg.b0.upaiyun.com/a87362da296c22ae174b64375a36cbc8637f99411236-i8wifg_fw658',
+	              'http://hbimg.b0.upaiyun.com/c427bbd74af50b21aeba870794e46b63367b5f6f1b57-DT6c1N_fw658'],
+	      img: 'http://hbimg.b0.upaiyun.com/bbba96a35c57e7dc1e53e7d278e44e1fd68beb09713c-Tf9wre_fw658'
+	    }},
+	    methods: {
+	      openTranslate: function () {
+	        var bundleUrl = this.$getConfig().bundleUrl
+	        console.log('hit', bundleUrl)
+	        var dirs = this.root.split('/');
+	        dirs.forEach(function(dir, index) {
+	          if (!dir) dirs.splice(index, 1);
+	        });
+	        var root = dirs.length > 0 ? dirs[0] : '';
+	        var subRoot = dirs.length > 1 ? dirs.slice(1).join('/') + '/' : '';
+	        console.log('open translate page')
+	        //如果pc调试，加载这个页面渲染
+	        var base = './index.html?page=./' + root + '/' + subRoot
+	        this.$openURL(base + this.item.name + '.js')
+	      },
+	    }
+	  }
+
+	;__weex_module__.exports.template = __weex_module__.exports.template || {}
+	;Object.assign(__weex_module__.exports.template, {
+	  "type": "div",
+	  "classList": [
+	    "bg"
+	  ],
+	  "children": [
+	    {
+	      "type": "text",
+	      "classList": [
+	        "title"
+	      ],
+	      "attr": {
+	        "value": "最常使用"
+	      }
+	    },
+	    {
+	      "type": "div",
+	      "classList": [
+	        "tools"
+	      ],
+	      "children": [
+	        {
+	          "type": "div",
+	          "repeat": {
+	            "expression": function () {return this.tools},
+	            "value": "tool"
+	          },
+	          "classList": [
+	            "tool"
+	          ],
+	          "events": {
+	            "click": function ($event) {this.openTranslate($event)}
+	          },
+	          "children": [
+	            {
+	              "type": "image",
+	              "style": {
+	                "width": 120,
+	                "height": 120
+	              },
+	              "attr": {
+	                "src": function () {return this.tool}
+	              }
+	            },
+	            {
+	              "type": "text",
+	              "style": {
+	                "width": 120,
+	                "height": 20
+	              },
+	              "classList": [
+	                "subtitle"
+	              ],
+	              "attr": {
+	                "value": function () {return '翻译'+this.$index}
+	              }
+	            }
+	          ]
+	        }
+	      ]
+	    }
+	  ]
+	})
+	;__weex_module__.exports.style = __weex_module__.exports.style || {}
+	;Object.assign(__weex_module__.exports.style, {
+	  "bg": {
+	    "borderRadius": 5,
+	    "borderWidth": 1,
+	    "borderColor": "#dadada",
+	    "backgroundColor": "#F7F7F7",
+	    "margin": 20,
+	    "width": 710
+	  },
+	  "title": {
+	    "fontSize": 30,
+	    "left": 30,
+	    "top": 30
+	  },
+	  "tools": {
+	    "flexDirection": "row",
+	    "justifyContent": "center",
+	    "paddingLeft": 20,
+	    "paddingRight": 20,
+	    "marginTop": 20
+	  },
+	  "tool": {
+	    "justifyContent": "center",
+	    "width": 120,
+	    "height": 200,
+	    "margin": 20
+	  },
+	  "subtitle": {
+	    "textAlign": "center",
+	    "fontSize": 18,
+	    "marginTop": 20,
+	    "marginBottom": 20
+	  }
+	})
+	})
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	;__weex_define__("@weex-component/logo", [], function(__weex_require__, __weex_exports__, __weex_module__){
+
+	;
+	  __weex_module__.exports = {
+	    data: function () {return {
+	      tools: ['http://hbimg.b0.upaiyun.com/72fa4f426ccae79476bbbc2ef723c388cf5f02ffeb96-zYEnwV_fw658',
+	              'http://hbimg.b0.upaiyun.com/028bb24be05e2eac593340d850457051df0ba5af9963-FALRwW_fw658',
+	              'http://hbimg.b0.upaiyun.com/a87362da296c22ae174b64375a36cbc8637f99411236-i8wifg_fw658',
+	              'http://hbimg.b0.upaiyun.com/c427bbd74af50b21aeba870794e46b63367b5f6f1b57-DT6c1N_fw658'],
+	      img: 'http://hbimg.b0.upaiyun.com/bbba96a35c57e7dc1e53e7d278e44e1fd68beb09713c-Tf9wre_fw658'
+	    }},
+	  }
+
+	;__weex_module__.exports.template = __weex_module__.exports.template || {}
+	;Object.assign(__weex_module__.exports.template, {
+	  "type": "div",
+	  "classList": [
+	    "bg"
+	  ],
+	  "children": [
+	    {
+	      "type": "div",
+	      "classList": [
+	        "logo"
+	      ],
+	      "children": [
+	        {
+	          "type": "image",
+	          "style": {
+	            "width": 400,
+	            "height": 400
+	          },
+	          "attr": {
+	            "src": "https://cdn4.iconfinder.com/data/icons/scripting-and-programming-languages/512/JQuery_logo-512.png"
+	          }
+	        }
+	      ]
+	    }
+	  ]
+	})
+	;__weex_module__.exports.style = __weex_module__.exports.style || {}
+	;Object.assign(__weex_module__.exports.style, {
+	  "bg": {
+	    "alignItems": "center"
+	  },
+	  "title": {
+	    "fontSize": 30,
+	    "left": 30,
+	    "top": 30
+	  },
+	  "tools": {
+	    "flexDirection": "row",
+	    "justifyContent": "center",
+	    "height": 300,
+	    "paddingLeft": 20,
+	    "paddingRight": 20
+	  },
+	  "tool": {
+	    "justifyContent": "center",
+	    "width": 120,
+	    "margin": 20
+	  },
+	  "subtitle": {
+	    "textAlign": "center",
+	    "fontSize": 20,
+	    "marginTop": 20,
+	    "marginBottom": 20
+	  }
+	})
+	})
+
+/***/ }
+/******/ ]);
